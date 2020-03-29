3bcd6f05137d6a68df91eec9f038932dd55d37e9
Taranov Sergiy <scorp1323@gmail.com>
2020-03-29 20:35:14 +0300

    Пррограмирование ждава

---
+++ _InWork/Aviasales_JS.ua/www/script.js
@@ -0,0 +1,32 @@
+
+
+const formSearch = document.querySelector('.form-search'),
+inputCitiesFrom = document.querySelector('input__cities-from'),
+dropdownCitiesFrom = document.querySelector('dropdown__cities-from'),
+inputCitiesTo = document.querySelector('input__cities-to'),
+dropdownCitiesTo = document.querySelector('input__cities-to'),
+inputDataDepart = document.querySelector('input__date-depart');
+
+
+const obj = {
+	key: 'value',
+	'key two': 'Value2',
+	key3: true,
+	key4: {
+		a: 1,
+		b: 'два',
+	},
+	KeyNext: ['array', 5, {a:1, b:2}, [true, false, 0]],
+}
+
+obj.key5 = 'Привет';
+
+//console.log(obj);
+
+let arr = [];
+
+console.log(arr);
+//49.14
+//https://www.youtube.com/watch?v=DWglfTOb8_k&t=1509s
+//Aviasales_JS.ua
+
