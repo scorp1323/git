929293703a45258e1513262d9a918383b7353f65
Taranov Sergiy <scorp1323@gmail.com>
2020-04-09 08:28:56 +0300

    Добавлена папка JS-Geym.ua

---
+++ _InWork/JS-Geym.ua/www/js/game.js
@@ -0,0 +1,15 @@
+var cvs = document.getElementById("canvas");
+
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
